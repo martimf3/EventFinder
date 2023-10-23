ef87585c9379d48652e915eccd944f3c2ce9d1b3
Vanina Rodrigues <vaninarodrigues@MacBook-Air-de-Vanina.local>
2023-10-23 18:41:15 +0100

    Conflitos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
-
-
 }
-
-
 android {
     namespace = "com.example.eventfinder"
     compileSdk = 34
@@ -53,9 +49,7 @@ android {
         }
     }
 }
-
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
