55b0060447aee452085fa2b747ea1b36c97fb161
André Freitas <amfreitas23@ua.pt>
2023-10-29 23:22:40 +0000

    Revert "Merge pull request #31 from martimf3/ui"
    This reverts commit 02fffbac7dc7c44a456ccb604c56eb4b19aa3af1, reversing
    changes made to 4422fc24330dcadefe0d8fb01670635589f0612c.

--- app/src/main/java/com/example/eventfinder/MainActivity.kt
+++ app/src/main/java/com/example/eventfinder/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.eventfinder
+
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -7,35 +8,8 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.LocationOn
-import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Badge
-import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -48,22 +22,10 @@ import com.example.eventfinder.navigation.MainNavigation
 import com.example.eventfinder.ui.screens.HomeScreen
 import com.example.eventfinder.ui.screens.ProfileScreen
 import com.example.eventfinder.ui.screens.SignInScreen
+import com.example.eventfinder.ui.screens.TestePage
 import com.google.android.gms.auth.api.identity.Identity
 import kotlinx.coroutines.launch
-import com.example.eventfinder.data.api.ticketMaster.*
-import com.example.eventfinder.ui.screens.EventListScreen
 
-
-data class BottomNavigationItem(
-    val title: String,
-    val route: String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector,
-    val hasNews: Boolean,
-    val badgeCount: Int? = null
-)
-
-@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     private val googleAuthUiClient by lazy {
         GoogleAuthUiClient(
@@ -75,127 +37,11 @@ class MainActivity : ComponentActivity() {
         val context = MyApplication.applicationContext()
         super.onCreate(savedInstanceState)
         setContent {
-
-            // VARIAVEIS
             val navController = rememberNavController()
-            var selectedItemIndex by rememberSaveable {
-                mutableStateOf(0)
-            }
-            var isLoggedIn by rememberSaveable {
-                mutableStateOf(false)
-            }
-
-            val items = listOf(
-                BottomNavigationItem(
-                    title = "Home",
-                    route = "home",
-                    selectedIcon = Icons.Filled.Home,
-                    unselectedIcon = Icons.Outlined.Home,
-                    hasNews = false,
-                ),
-                BottomNavigationItem(
-                    title = "Location",
-                    route = "location",
-                    selectedIcon = Icons.Filled.LocationOn,
-                    unselectedIcon = Icons.Outlined.LocationOn,
-                    hasNews = false,
-                ),
-                BottomNavigationItem(
-                    title = "WishList",
-                    route = "wishList",
-                    selectedIcon = Icons.Filled.Favorite,
-                    unselectedIcon = Icons.Outlined.FavoriteBorder,
-                    hasNews = false,
-                    badgeCount = 3,
-                ),
-                BottomNavigationItem(
-                    title = "Settings",
-                    route = "Profile",
-                    selectedIcon = Icons.Filled.Settings,
-                    unselectedIcon = Icons.Outlined.Settings,
-                    hasNews = true,
-                ),
-
-
-                )
-
-
-
-            if (isLoggedIn) {
-                Scaffold(
-                    bottomBar = {
-
-                        NavigationBar() {
-                            items.forEachIndexed { index, item ->
-                                NavigationBarItem(
-                                    selected = selectedItemIndex == index,
-                                    onClick = {
-                                        selectedItemIndex = index
-                                        navController.navigate(item.route)
-                                    },
-                                    label = {
-                                        Text(text = item.title)
-                                    },
-                                    icon = {
-                                        BadgedBox(
-                                            badge = {
-                                                if (item.badgeCount != null) {
-
-                                                    Badge {
-                                                        Text(text = item.badgeCount.toString())
-                                                    }
-                                                } else if (item.hasNews) {
-                                                    Badge()
-                                                }
-
-                                            }
-                                        ) {
-                                            Icon(
-                                                imageVector =
-                                                if (index == selectedItemIndex) {
-                                                    item.selectedIcon
-                                                } else item.unselectedIcon,
-                                                contentDescription = item.title
-
-                                            )
-                                        }
-                                    }
-                                )
-                            }
-                        }
-
-                    }
-
-                ) { paddingValues ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(paddingValues)
-                            .verticalScroll(rememberScrollState())
-                    ) {
-
-                    }
-                }
-            }
-
-
-
-            NavHost(navController = navController, startDestination = "distinction") {
-
-
-                composable("distinction") {
-
-                    if (isLoggedIn) {
-                        navController.navigate("home")
-                    }
-
-                    if(!isLoggedIn){
-                        navController.navigate("sign_in")
-                    }
-
-                }
+            NavHost(navController = navController, startDestination = "sign_in" ){
 
                 // SIGN IN NAVBAR
+
                 composable("sign_in") {
 
                     val viewModel = viewModel<SignInViewModel>()
@@ -211,14 +57,14 @@ class MainActivity : ComponentActivity() {
                                     viewModel.onSignInResult(signInResult)
 
 
+
                                 }
                             }
                         }
 
                     )
-                    LaunchedEffect(key1 = state.isSignInSuccessful) {
-                        if (state.isSignInSuccessful) {
-                            isLoggedIn = true
+                    LaunchedEffect(key1 = state.isSignInSuccessful ){
+                        if (state.isSignInSuccessful){
                             Toast.makeText(
                                 applicationContext,
                                 "Sign In Successful",
@@ -247,10 +93,7 @@ class MainActivity : ComponentActivity() {
                 }
 
                 //SIGN OUT NAVBAR
-                composable("profile") {
-                    val viewModel = viewModel<SignInViewModel>()
-                    val state by viewModel.state.collectAsStateWithLifecycle()
-
+                composable("profile"){
                     ProfileScreen(
                         navController,
                         userData = googleAuthUiClient.getSignedInUser(),
@@ -262,32 +105,23 @@ class MainActivity : ComponentActivity() {
                                     applicationContext,
                                     "Signed Out",
                                     Toast.LENGTH_LONG
-                                ).show()
-                                if(!state.isSignInSuccessful){
-                                    isLoggedIn = false
-                                }
+                                    ).show()
                                 navController.navigate("sign_in")
 
-                            }
+                           }
 
                         }
-                    )
+                   )
+
                 }
-                
+
                 //HOME NAVBAR -> Should Be Placed a Navbar here, with our homepage, and the navBar on Bottom
-
                 composable("home"){
-
                     HomeScreen(navController)
                 }
-                
                 // Teste Navegacao da Aplicacao
-                composable("location") {
-
-                }
-
-                composable("wishList") {
-
+                composable("location"){
+                    TestePage("Random",navController)
                 }
 
                 // Sing Up With Email
@@ -295,13 +129,9 @@ class MainActivity : ComponentActivity() {
                 {
 
                 }
-
             }
-
             MainNavigation()
         }
     }
 }
-
-
 
--- app/src/main/java/com/example/eventfinder/navigation/NavHost.kt
+++ app/src/main/java/com/example/eventfinder/navigation/NavHost.kt
@@ -5,14 +5,12 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.eventfinder.auth.*
 import com.example.eventfinder.auth.googleauth.sign_in.SignInState
 import com.example.eventfinder.ui.screens.HomeScreen
 import com.example.eventfinder.ui.screens.SignInScreen
 import com.example.eventfinder.ui.screens.TestePage
-
-
 @Composable
+
 fun MainNavigation() {
     val navController = rememberNavController()
 
