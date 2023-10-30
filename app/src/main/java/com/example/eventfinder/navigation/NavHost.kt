c6942ffd2a86eb31f40caf422c53ffec25426c59
martimf3 <martimmrcf1@gmail.com>
2023-10-30 00:07:47 +0000

    Revert "Implementacao de uma bottom navbar"
    This reverts commit 3ca671d540c035a3943b26434d949fbc326f5ebd.
    
    # Conflicts:
    #	app/src/main/java/com/example/eventfinder/MainActivity.kt
    #	app/src/main/java/com/example/eventfinder/navigation/NavHost.kt
    #	app/src/main/java/com/example/eventfinder/ui/screens/HomeScreen.kt

--- app/src/main/java/com/example/eventfinder/navigation/NavHost.kt
+++ app/src/main/java/com/example/eventfinder/navigation/NavHost.kt
@@ -5,7 +5,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
-import com.example.eventfinder.ui.screens.TestePage
+
+
+import com.example.eventfinder.auth.*
 @Composable
 
 fun MainNavigation() {
@@ -15,9 +17,7 @@ fun MainNavigation() {
         composable("home") {
 
         }
-        composable("testepage") {
-            TestePage("parametro random", navController)
-        }
+
     }
 }
 
--- app/src/main/java/com/example/eventfinder/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/eventfinder/ui/screens/HomeScreen.kt
@@ -1,135 +1,88 @@
 package com.example.eventfinder.ui.screens
 
-import android.location.Location
 import androidx.compose.foundation.layout.Arrangement
+import android.location.Location
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.eventfinder.MyApplication
-import com.example.eventfinder.data.api.ticketMaster.GetEvents
 import com.example.eventfinder.location.LocationService
 
+data class BottomNavigationItem(
+    val title: String,
+    val route: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(navController: NavController) {
-    val context = MyApplication.applicationContext()
-    val locationService = LocationService(context)
-    Column(
-        modifier = Modifier.padding(50.dp)
+
+fun HomeScreen(
+    navController: NavController
+) {
+
+    Column(modifier = Modifier
+        .padding(50.dp),
+
     ) {
-        Row(
-            modifier = Modifier
-                .padding(5.dp)
-                .fillMaxSize(),
-            verticalAlignment = Alignment.Bottom
-        ) {
-            Button(
-                onClick = {},
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth()
-            ) {
-                Text(text = "Menu")
-            }
-        }
-
-        Column {
-            Row(
-                modifier = Modifier
-                    .padding(5.dp)
-                    .fillMaxSize(),
-                verticalAlignment = Alignment.Bottom,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Button(onClick = {
-                    navController.navigate("profile")
-                }) {
-                    Text(text = "Profile")
-                }
-
-                Button(onClick = {
-                    locationService.getLastLocation(object : LocationService.LocationCallback {
-                        override fun onLocationResult(location: Location?) {
-                            if (location != null) {
-                                // Handle the retrieved location her
-                                GetEvents(location, 100) { eventData ->
-                                    if (eventData != null) {
-                                        print(eventData.count())
-                                    } else {
-                                        // Handle the case where event data retrieval fails
-                                        println("Error retrieving events list")
-                                    }
-                                }
-                            } else {
-                                println("Localização não disponivel")
-                            }
-                        }
-
-                        override fun onError(errorMessage: String) {
-                            println("Erro ao ir buscar a localização atual")
-                        }
-                    })
-                }) {
-                    Text(text = "Search Events")
-                }
-
-                Button(onClick = {
-                    // TODO: Implement WishList functionality
-                }) {
-                    Text(text = "WishList")
-                }
-
-                Button(onClick = {
-                    // TODO: Implement Wallet functionality
-                }) {
-                    Text(text = "Wallet")
-                }
-            }
-        }
-    }
-
-    Column {
         Row(
             modifier = Modifier
                 .padding(5.dp)
                 .fillMaxSize(),
             verticalAlignment = Alignment.Bottom,
             horizontalArrangement = Arrangement.SpaceEvenly
+
         ) {
-
-
-            Button(onClick = {
-                navController.navigate("profile")
-            }) {
-                Text(text = "Profile")
-            }
-
-            Button(onClick = { navController.navigate("location") }) {
-                Text(text = "Location ")
-            }
-
             Button(onClick = { /*TODO*/ }) {
-                Text(text = "WishList")
+                Text(text = "Menu")
 
             }
-
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Wallet")
-
-            }
-
-
         }
 
     }
+
+
 }
 
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(navController = rememberNavController())
+
+}
